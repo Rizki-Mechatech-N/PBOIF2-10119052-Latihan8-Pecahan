fc00cd9cffe49980f90100188a4565cbf820c9a8
Kiki <Kiki@Kiki-PC>
2020-10-18 11:15:35 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan8/pecahan/PBOIF210119052Latihan8Pecahan.java
@@ -0,0 +1,26 @@
+package pboif2.pkg10119052.latihan8.pecahan;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * pecahan
+ * 
+ */
+public class PBOIF210119052Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.1234567890;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
